2c62b93956f12584c85be2c5174939ef4ad6d11b
Surya <37679899+srcode5@users.noreply.github.com>
2018-03-24 15:19:51 -0700

    hi

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // Injected JavaScript goes nere
 // for this file put extension.js
 function start(){
-	chrome.browserAction.onClicked.addListener(function (tab){
-	// for the current tab, inject the "extension.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'extension.js'
-	});
+	"content_scripts": [
+		  {
+			"matches": [
+			  "<all_urls>"
+			],
+			"js": ["content.js"]
+		  }
+	]
 });
 	checkHatefulDegree(new HateValue({ s }));
 }
