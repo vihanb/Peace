8e1c9cbe9317923694aa6329158bfd3e84b6c7a1
Surya <37679899+srcode5@users.noreply.github.com>
2018-03-24 14:37:00 -0700

    Add manifest.json

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 // JavaScript source code
 function start(){
-	
+	chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+  });
 }
