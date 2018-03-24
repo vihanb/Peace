513734d68b3df119646a7f158b9a31803b59b459
Surya <37679899+srcode5@users.noreply.github.com>
2018-03-24 16:06:55 -0700

    add reddit.js

--- reddit.js
+++ reddit.js
@@ -3,9 +3,13 @@ function start(){
 	var htmlComments = document.getElementsByClassName("comment");
 	var objs = [];
 	for (var i = 0; i < htmlComments.length; i++) {
-		objs.push({
-			element: htmlComments[i],
-			text: htmlComments[i].getElementsByClassName("md")[0].textContent
-		});
+		if(format(json) >= 0.6){
+			console.log("Are you sure you want to display this hateful comment?");
+		}else{
+			objs.push({
+				element: htmlComments[i],
+				text: htmlComments[i].getElementsByClassName("md")[0].textContent
+			});
+		}
 	}
 }
