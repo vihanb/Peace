0d26fd2a809fd3c35fd79c39c407a8cc62cdf174
prgoradia <37748035+prgoradia@users.noreply.github.com>
2018-03-24 16:22:50 -0700

    Add youtube comments check

---
+++ youtube.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+function start(){
+	var comm = document.getElementsByClassName("comment");
+	var arr = [];
+	for(var i = 0; i < comm.length; i++){
+		if(format(json) >= 0.6){
+		<span id = #warningMessage>
+			"Are you sure you want to display this hateful comment."
+		</span>
+		document.body.innerHTML = document.body.innerHTML.replace(htmlComments[i], #warningMessage);
+		}else{
+			objs.push({
+				element: htmlComments[i],
+				text: htmlComments[i].getElementsByClassName("md")[0].textContent
+			});
+		}
+	}
+}
