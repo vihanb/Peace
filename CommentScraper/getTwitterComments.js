66e4645148942c60a60f2f379cb5003434670eb4
CrazyGuy108 <taylor@hansenfamily.us>
2018-03-24 18:17:08 -0700

    Implement Youtube comment scraping

---
+++ CommentScraper/getTwitterComments.js
@@ -0,0 +1,11 @@
+import Comment from  './Comment'
+
+/**
+ * Scrapes a Twitter website for comments.
+ *
+ * @return {Comment[]} Twitter comments.
+ * @private
+ */
+export default function getTwitterComments() {
+    return [];
+}
